6864eff06e133550c17b964a7b684557b8ebec7c
MKS-students <mks.students@hackreactor.com>
2015-07-09 17:03:03 -0500

    Sequelize Refactor

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,23 +1,100 @@
 var models = require('../models');
 var bluebird = require('bluebird');
 
+var Sequelize = require("sequelize");
+var sequelize = new Sequelize("chat", "root", "");
+
+var User = sequelize.define('User', {
+  username: Sequelize.STRING
+});
+
+var Message = sequelize.define('Message', {
+  message: Sequelize.STRING,
+  roomname: Sequelize.STRING
+})
+//Create associations
+Message.belongsTo(User)
+User.hasMany(Message)
+//Create database table
+User.sync()
+Message.sync()
 
 
 module.exports = {
   messages: {
     get: function (req, res) {
-      models.messages.get(req,res);
-    }, // a function which handles a get request for all messages
+      Message.findAll({}).then(function(messages){
+        res.send(messages);
+      })
+    },
     post: function (req, res) {
-      models.messages.post(req,res);
-      res.end();
-    } // a function which handles posting a message to the database
+      var args = [req.body.username, req.body.message, req.body.roomname]
+      User.count({username: req.body.username}).then(function(number){
+      console.log('in post')
+        if (number === 0) {
+          var newUser = User.build({username:req.body.username})
+          newUser.save().then(function(){
+            User.find({username: args[0]}).then(function(user){
+              var newMessage = Message.build({userid:user.id, message: args[1], roomname: args[2]})
+              newMessage.save().then(function(){
+                res.end();
+              })
+            })
+          })
+        } else {
+          User.find({username: args[0]}).then(function(user){
+            var newMessage = Message.build({userid:user.id, message: args[1], roomname: args[2]})
+            newMessage.save().then(function(){
+              res.end();
+            })
+          })
+        }
+      })
+    }
   },
 
   users: {
-    // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      User.findAll({}).then(function(users){
+        res.send(users);
+      })
+    },
+    post: function (req, res) {
+      User.count({username: req.body.username}).then(function(number){
+        if (number === 0) {
+          var newUser = User.build({username:req.body.username})
+          newUser.save().then(function(){
+            res.end()
+          })
+        }
+      })
+    }
   }
 };
+
+
+
+
+// module.exports = {
+//   messages: {
+//     get: function (req, res) {
+//       models.messages.get(req,res);
+//     }, // a function which handles a get request for all messages
+//     post: function (req, res) {
+//       models.messages.post(req,res);
+
+//     } // a function which handles posting a message to the database
+//   },
+
+//   users: {
+//     // Ditto as above
+//     get: function (req, res) {
+//       models.users.get(req, res);
+//     },
+//     post: function (req, res) {
+//       models.users.get(req, res);
+//       res.end();
+//     }
+//   }
+// };
 
--- server/models/index.js
+++ server/models/index.js
@@ -40,28 +40,49 @@ var getRoomId = function(room, callback){
   });
 };
 
+var headers = {};
+headers["access-control-allow-origin"] = "*";
+headers["access-control-allow-methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+headers["access-control-max-age"] = '86400'; // 24 hours
+headers["access-control-allow-headers"] = "*";
+
 module.exports = {
   messages: {
     get: function (req, res) {
+      res.writeHead(200,headers);
+      console.log("GET" + headers)
       db.query('SELECT * FROM messages',
       function(err, results, fields) {
-        res.send(results);
+        res.end(JSON.stringify(results));
       })
 
     }, // a function which produces all the messages
     post: function (req, res) {
+      res.writeHead(200,headers);
+      console.log('POST'+headers);
       getUserId(req.body.username, function(uId){
         getRoomId(req.body.roomname, function(rId){
           putMessage(uId, req.body.message, rId);
         })
       });
-    } // a function which can be used to insert a message into the database
-  },
-
-  users: {
-    // Ditto as above.
-    get: function (req, res) {},
-    post: function (req, res) {}
+      res.end();
+    }, // a function which can be used to insert a message into the database
+    options: function(req, res){
+      res.writeHead(200, headers);
+      res.end();
+    }
   }
+
+  // users: {
+  //   // Ditto as above.
+  //   get: function (req, res) {
+  //     db.query('SELECT * FROM users', function(err, results, fields){
+  //       res.send(results);
+  //     })
+  //   },
+  //   post: function (req, res) {
+  //     getUserId(req.body.username, function(){});
+  //   }
+  // }
 };
 
