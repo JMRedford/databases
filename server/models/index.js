d521ef431f53e922ead1aec6ce7d5fa146699914
MKS-students <mks.students@hackreactor.com>
2015-07-08 19:49:59 -0500

    Get and Post running properly

--- server/models/index.js
+++ server/models/index.js
@@ -1,18 +1,67 @@
 var db = require('../db');
 
 
+var putMessage = function(uId, text, rId){
+  console.log(uId);
+  var date = new Date();
+  db.query('INSERT INTO messages VALUES (' + uId + ', \'' +
+                                             text + '\', ' +
+                                             rId + ', \'' +
+  date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate() + '\')');
+};
 
+var getUserId = function(name, callback){
+  db.query('SELECT u_id FROM users WHERE name=\''+name+'\'',
+  function(err, results, fields){
+    if (results.length === 0){
+      db.query('INSERT INTO users (name) VALUES (\''+name+'\')', function(){
+        db.query('SELECT u_id FROM users WHERE name=\''+name+'\'', function(err2, results2, fields2){
+          callback(results2[0].u_id);
+        });
+      });
+    } else {
+      callback(results[0].u_id);
+    }
+  });
+};
+
+var getRoomId = function(room, callback){
+  db.query('SELECT r_id FROM rooms WHERE name=\''+room+'\'',
+  function(err, rows, fields){
+    if (rows.length === 0){
+      db.query('INSERT INTO rooms (name) VALUES (\''+room+'\')', function(){
+        db.query('SELECT r_id FROM rooms WHERE name=\''+room+'\'', function(err2, results2, fields2){
+          callback(results2[0].r_id);
+        });
+      });
+    } else {
+      callback(rows[0].r_id);
+    }
+  });
+};
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (req, res) {
+      db.query('SELECT * FROM messages',
+      function(err, results, fields) {
+        res.send(results);
+      })
+
+    }, // a function which produces all the messages
+    post: function (req, res) {
+      getUserId(req.body.username, function(uId){
+        getRoomId(req.body.roomname, function(rId){
+          putMessage(uId, req.body.message, rId);
+        })
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (req, res) {},
+    post: function (req, res) {}
   }
 };
 
--- server/schema.sql
+++ server/schema.sql
@@ -3,11 +3,25 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
+  u_id int,
+  text varchar(180),
+  r_id int,
+  time date
   /* Describe your table here.*/
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  r_id int NOT NULL AUTO_INCREMENT,
+  name varchar(20),
+  PRIMARY KEY (r_id)
+);
 
+CREATE TABLE users (
+  u_id int NOT NULL AUTO_INCREMENT,
+  name varchar(20),
+  PRIMARY KEY (u_id)
+);
 
 
 
